03e919918519e94674b64a18174b5906f8671f3a
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-01-31 13:19:21 +0200

    unfinished

--- main.py
+++ main.py
@@ -10,70 +10,42 @@ exit_button = Button(window, text="Exit", command=window.destroy)
 exit_button.place(x=450,y=255)
 
 
+
+e2_value = StringVar()
+e2 = Entry(window, textvariable=e2_value)
+e2.grid(row=0, column=1, pady=10, padx=10)
+
+i = Label(window, text='inch', bg="light gray")
+i.grid(row=2, column=0, pady=10, padx=10)
+
+t = Text(window, height=1, width=15)
+t.grid(row=2, column=1, pady=10, padx=10)
+t.delete("1.0", END)
+
 def conversion_to_inches():
     if x.lower() == "m":
+        length_in_meters = float(e2_value.get())
+        inch = length_in_meters * 39.37
         e1 = Label(window, text="Enter length in meters", bg="light gray")
         e1.grid(row=0, column=0, padx=10, pady=10)
 
-        e2_value = StringVar()
-        e2 = Entry(window, textvariable=e2_value)
-        e2.grid(row=0, column=1, pady=10, padx=10)
-
-        # inch = float(e2_value.get()) * 39.37
-        i = Label(window, text='inch', bg="light gray")
-        i.grid(row=2, column=0, pady=10, padx=10)
-
-        t = Text(window, height=1, width=15)
-        t.grid(row=2, column=1, pady=10, padx=10)
         t.delete("1.0", END)
-        # t.insert(END, inch)
+        t.insert(END, inch)
+
 
     elif x.lower() == "i":
+        length_in_inches = float(e2_value.get())
+        meters = length_in_inches * 0.0254
         e1 = Label(window, text="Enter length in inches", bg="light gray")
         e1.grid(row=0, column=0, padx=10, pady=10)
 
-        e2_value = StringVar()
-        e2 = Entry(window, textvariable=e2_value)
-        e2.grid(row=0, column=1, pady=10, padx=10)
-
-        # inch = float(e2_value.get()) * 39.37
-        i = Label(window, text='inch', bg="light gray")
-        i.grid(row=2, column=0, pady=10, padx=10)
-
-        t = Text(window, height=1, width=15)
-        t.grid(row=2, column=1, pady=10, padx=10)
         t.delete("1.0", END)
-        # t.insert(END, inch)
-
-    b = Button(window, text="Convert", command=conversion_to_inches)
-    b.grid(row=1, column=1, padx=10, pady=10)
+        t.insert(END, meters)
 
 
-
-conversion_to_inches()
+b = Button(window, text="Convert", command=conversion_to_inches)
+b.grid(row=1, column=1, padx=10, pady=10)
 
 window.mainloop()
 
-
-
-
-
-
-# def conversion():
-#     x = input("Do you want meters to inches[m] or inches to meters[i] ? ")
-#     x = x.lower()
-#
-#     if x == "m":
-#         default = 39.3700787
-#         calculate = int(input("How much meters do you want to convert ? ")) * default
-#         print(calculate, "inches")
-#     elif x == "i":
-#         default2 = 0.0254
-#         calculate2 = int(input()) * default2
-#         print(calculate2, "meters")
-#     else:
-#         print("Please enter a valid letter!")
-#
-#
-# conversion()
 
