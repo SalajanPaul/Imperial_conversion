4fd3ddfda1fcbff316103add80560fd5446427bc
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-01-31 13:44:51 +0200

    unfinished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 x = input("Do you want to convert meters to inches[m] or inches to meters[i]? ")
 
@@ -6,9 +7,6 @@ x = input("Do you want to convert meters to inches[m] or inches to meters[i]? ")
 window = Tk()
 window.title("Meters to inches converter")
 window.geometry("500x300+550+355")
-exit_button = Button(window, text="Exit", command=window.destroy)
-exit_button.place(x=450,y=255)
-
 
 
 e2_value = StringVar()
@@ -17,6 +15,9 @@ e2.grid(row=0, column=1, pady=10, padx=10)
 
 i = Label(window, text='inch', bg="light gray")
 i.grid(row=2, column=0, pady=10, padx=10)
+
+e1 = Label(window, text="Enter length in meters", bg="light gray")
+e1.grid(row=0, column=0, padx=10, pady=10)
 
 t = Text(window, height=1, width=15)
 t.grid(row=2, column=1, pady=10, padx=10)
@@ -36,8 +37,11 @@ def conversion_to_inches():
     elif x.lower() == "i":
         length_in_inches = float(e2_value.get())
         meters = length_in_inches * 0.0254
-        e1 = Label(window, text="Enter length in inches", bg="light gray")
-        e1.grid(row=0, column=0, padx=10, pady=10)
+        e_inches = Label(window, text="Enter length in inches", bg="light gray")
+        e_inches.grid(row=0, column=0, padx=10, pady=10)
+
+        i = Label(window, text='meters', bg="light gray")
+        i.grid(row=2, column=0, pady=10, padx=10)
 
         t.delete("1.0", END)
         t.insert(END, meters)
@@ -45,6 +49,14 @@ def conversion_to_inches():
 
 b = Button(window, text="Convert", command=conversion_to_inches)
 b.grid(row=1, column=1, padx=10, pady=10)
+
+def on_closing():
+    if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
+        window.destroy()
+
+
+exit_button = Button(window, text="Exit", command=on_closing)
+exit_button.place(x=450,y=255)
 
 window.mainloop()
 
